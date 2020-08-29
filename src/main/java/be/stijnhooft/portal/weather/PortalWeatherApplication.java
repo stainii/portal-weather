d246409b3a4121a50a1f06a7fdaaa8168e1ebc09
stainii <stijnhooft@hotmail.com>
2020-08-29 18:59:55 +0200

    spring boot template

---
+++ src/main/java/be/stijnhooft/portal/weather/PortalWeatherApplication.java
@@ -0,0 +1,13 @@
+package be.stijnhooft.portal.weather;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalWeatherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalWeatherApplication.class, args);
+	}
+
+}
---
+++ src/test/java/be/stijnhooft/portal/weather/PortalWeatherApplicationTests.java
@@ -0,0 +1,13 @@
+package be.stijnhooft.portal.weather;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalWeatherApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
